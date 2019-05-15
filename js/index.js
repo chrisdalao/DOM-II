abd24a0320c56da283fb1ac26457e7a6af251e86
chrisdalao <chrisdalao@gmail.com>
2019-05-14 20:38:02 -0700

    added - alert div, reload action onece at bottom

--- js/index.js
+++ js/index.js
@@ -3,62 +3,76 @@
 const navBtnArr = document.querySelectorAll("a");
 const imagesArr = document.querySelectorAll("img");
 const pTagArr = document.querySelectorAll("p");
+const containerDiv = document.querySelector(".home");
+const alertDiv = document.createElement("div");
+const moreBtn = document.createElement("button");
+
+
+window.addEventListener('scroll', function () {
+    const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+    const scrolled = window.scrollY;
+
+    if (scrolled === scrollable && document.documentElement.scrollHeight <= 1980) {
+        alertDiv.setAttribute("class", "bottom-alert-div");
+        alertDiv.textContent = "You've reached the bottom of the page! CONGRATULATIONS!";
+        alertDiv.classList.toggle('large');
+        containerDiv.appendChild(alertDiv);
+    }
+
+})
 //Event Listeners
 
 
-navBtnArr.forEach(function(navBtn){
-    navBtn.addEventListener('click', function(event) {
+navBtnArr.forEach(function (navBtn) {
+    navBtn.addEventListener('click', function (event) {
         navBtn.style.color = "white";
         navBtn.style.padding = "10px";
         navBtn.style.backgroundColor = "#17A2B8";
         navBtn.style.borderRadius = "4px";
         event.preventDefault();
-      })
+    })
 
-    
-    navBtn.addEventListener('mouseover',function(){
+
+    navBtn.addEventListener('mouseleave', function () {
         navBtn.style.color = "#212529";
         navBtn.style.padding = "0px";
         navBtn.style.backgroundColor = "white";
         navBtn.style.borderRadius = "0px";
     })
 
-    
+
 })
 
-imagesArr.forEach(function(image){
-    // image.addEventListener('dragstart', function(){
-    //     image.style.borderRadius = "1000px";
-    // })
+imagesArr.forEach(function (image) {
 
-    image.addEventListener('dragend', function(){
+    image.addEventListener('dragend', function () {
         image.style.display = "none";
     })
 
-    window.addEventListener('scroll', function(){
-        const scrollable = document.documentElement.scrollHeight - window.innerHeight;
-        const scrolled = window.scrollY;
+    image.addEventListener('dblclick', function () {
+        image.classList.toggle('large');
+    })
 
-        if(scrolled === scrollable && image.style.display === "none"){
-            image.style.display = "block";
-        }
-        
+    alertDiv.addEventListener('dblclick', function () {
+        window.scrollTo(0, 0);
+        location.reload();
+        image.style.display = "block";
     })
 
 })
 
 
-pTagArr.forEach(function(pTag){
+pTagArr.forEach(function (pTag) {
 
-    pTag.addEventListener('dblclick', function (e) {
+    pTag.addEventListener('dblclick', function () {
         pTag.classList.toggle('large');
-      });
+    });
 
-    pTag.addEventListener('mouseleave', function(){
-        if(pTag.classList.toggle('large')){
+    pTag.addEventListener('mouseleave', function () {
+        if (pTag.classList.toggle('large')) {
             pTag.classList.toggle('large');
         };
-    })
+    });
 
 });
 
